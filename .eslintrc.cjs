9a0be075069faefe56b737fcb1326fce4e6d5b7b
Vlad Zinchenko <vlad.zinchenko@bndigital.co>
2024-03-04 20:38:46 +0200

    added extends

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,9 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    "@wemake-services/typescript/recommended",
+    "@wemake-services/javascript",
+    "@wemake-services/stylelint-config-scss"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
